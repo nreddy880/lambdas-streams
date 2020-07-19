33d4891c952a8861893196d30b7219cb5a883e07
Niharika <HP CR2000TU@LAPTOP-RIF3ITIS>
2020-07-19 19:56:04 +0530

    commited

---
+++ lambda/src/main/java/epam/lambda/CombinationOption.java
@@ -0,0 +1,14 @@
+package epam.lambda;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public enum CombinationOption {
+	    INCLUDE_DUPLICATED,
+	    EXCLUDE_DUPLICATED
+	}
